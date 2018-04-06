897174c82632ee2584dd69fedb608fc5e9fb82da
Hugo <hugochen1212@gmail.com>
2018-04-06 00:25:30 -0700

    able to get data from API with axios

--- screens/welcomeScreen.js
+++ screens/welcomeScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import Slides from '../components/Slides';
+import axios from 'axios';
 
 const SLIDE_DATE = [
   {text: 'Welcome to cCLove App', color: '#03A9F4'},
@@ -13,6 +14,14 @@ class WelcomeScreen extends Component {
     this.props.navigation.navigate('Home');
   }
 
+   componentDidMount() {
+    axios.get("http://api.petfinder.com/pet.get?key=dbeaf4be86fcf7cfe093d67eef6689b7&id=38356287&format=json").then( response => {
+      console.log(response);
+     }).catch(error => {
+        console.log(error);
+     });
+
+  }
 
 
   render() {
